d1f2fb787bd3063039a5e5dad7b4be4c7209aa40
KJ Rideout <kjrideout@gmail.com>
2021-05-21 15:01:02 -0400

    dynamic maze redraw

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,18 +1,60 @@
 function onLoad() {
-  let cell_data = []
-
-  let cellHeight = 60;
-  let ribbonWidth = document.querySelector("#ribbonMaze").offsetWidth;
-  let numCellsWide = Math.floor(ribbonWidth / (cellHeight * 0.97) );
-
-  //insert cells divs
   
+  reshapeMaze();
+  window.addEventListener('resize', reshapeMaze);
+
+  function reshapeMaze() {
+
+    const cellHeight = 60; //learn to pass this
+    const newWidth = window.innerWidth;
+    const newHeight = window.innerHeight;
+    
+    // record these to the hidden #dataDiv
+    document.querySelector('#width').textContent = newWidth;
+    document.querySelector('#height').textContent = newHeight;
+
+    let numCellsWide = Math.floor(newWidth / (cellHeight * 0.97));
+
+    //track for reference
+    console.log(numCellsWide);
+
+
+
+
+  }
+
+}
+
+
+
+
+
+  //window.addEventListener('resize', reportWindowSize);
+ /*
+  function reportWindowSize() {  //will pass variable
+    const cellHeight = 60; //learn to pass this value
+
+    widthOutput.textContent = window.innerWidth;
+    heightOutput.textContent = window.innerHeight;
+  }
+  
+
+  window.onresize = reportWindowSize();
+  console.log(reportWindowSize());
+
+  const dataDiv = document.getElementById("hiddenTop");
+  // dataDiv.style.display = "none";
+
+
+  //let numCellsWide = Math.floor(widthOutput / (cellHeight * 0.97) );
+
+  
+
+  //insert cell divs
   const newMazeCells = document.createElement('div');
   newMazeCells.id = 'frame';
   newMazeCells.className ='grid-child testBorder';
 
-  console.log(numCellsWide);
+  // console.log(numCellsWide);
 
-
-}
-
+  */

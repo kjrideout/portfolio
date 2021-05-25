75ef491ad05963135b33dacdad5d2fb3ccfb46e0
KJ Rideout <kjrideout@gmail.com>
2021-05-25 00:01:34 -0400

    css style changes

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,65 @@
 function onLoad() {
+
+  window.addEventListener('resize', function() {location.reload();} );
   
+  const cellHeight = 60; //learn to pass this
+  const xSize = Math.floor(document.getElementById('gameGrid').offsetWidth / (cellHeight * 0.95));
+  const ySize = Math.floor(document.getElementById('gameGrid').offsetHeight / cellHeight);
+  const cellDiv = document.getElementById('gameGrid');
+  
+  cellDiv.style.gridTemplateColumns = "repeat(" + xSize + ", 1fr)";
+  
+  for (let y = 0; y < ySize; y++) {
+    for(let x = 0; x < xSize; x++) {
+      const newCell = document.createElement('div');
+      newCell.id = "cell-" + x +"x-" + y +"y";
+      newCell.className = "grid-child cell";
+      //newCell.innerHTML = "x";
+      cellDiv.appendChild(newCell);
+    }
+  }
+
+  
+  // 
+  for (let i = 0; i < cellDiv.childElementCount; i++) {
+    cellDiv.children[i].innerHTML = "x"
+  }
+
+
+
+
+  // array of 4 top right bottom left
+
+}
+
+
+
+
+
+  /*
+  stickfigHTML = 
+    '<div id=cellGrid class="grid-container"' +
+    'style="display: grid; grid-gap: 0px; grid-template-columns: repeat(17, 1fr);">' +
+      '<div id="cell-0" class="grid-child cell">'
+        '<a href="https://github.com/kjrideout">'
+          '<img src="img/stickfig.png" id="stickfig">'
+        '</a>'
+      '</div>'
+      '<!--remaining divs for this element will be inserted by js-->' 
+    '</div>';
+  */
+
+  //let startHTML = document.cloneNode
+
+  //while(cellContainer.firstChild) {
+  //  cellContainer.removeChild(cellContainer.firstChild);
+  //}
+
+
+
+  /*
+
+
   redrawMaze();
   window.addEventListener('resize', reloadPage);
 
@@ -38,4 +98,5 @@ function onLoad() {
     }
     console.log(cellGridContent);
   }
-}
+
+  */
